63f21ae0d55a2344b443b989ca826667e6deee8d
DDU <ddu6@protonmail.com>
2021-12-27 01:30:38 +0800

    update

--- dist/lib/css.d.ts
+++ dist/lib/css.d.ts
@@ -1,2 +1,2 @@
-export declare const line = "pre>.line {\n    display: block;\n    white-space: pre;\n}\n\npre>.line>.content {\n    display: inline-block;\n    white-space: pre-wrap;\n}";
-export declare const all = "pre>.line {\n    display: block;\n    white-space: pre;\n}\n\npre>.line>.content {\n    display: inline-block;\n    white-space: pre-wrap;\n}";
+export declare const line = "pre>.line {\n    display: flex;\n    white-space: pre;\n}\n\npre>.line>.content {\n    display: inline-block;\n    white-space: pre-wrap;\n}";
+export declare const all = "pre>.line {\n    display: flex;\n    white-space: pre;\n}\n\npre>.line>.content {\n    display: inline-block;\n    white-space: pre-wrap;\n}";
