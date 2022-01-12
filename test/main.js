b3c58981856d651a5002f97989f6fd3da38566aa
DDU <ddu6@protonmail.com>
2022-01-12 20:27:57 +0800

    update

--- test/main.js
+++ test/main.js
@@ -1,4 +1,6 @@
 import {extractLangInfoArrayFromVSCEURLs, extractThemeFromVSCTURLs, Highlighter} from '../mod.js'
+document.documentElement.style.background = '#1E1E1E'
+document.documentElement.style.color = '#D4D4D4'
 const example = `# Test
 
 \`\`\`ts
@@ -27,8 +29,6 @@ langInfoArray.push({
 const theme = await extractThemeFromVSCTURLs([
     'dark_plus.json'
 ], 'https://cdn.jsdelivr.net/gh/microsoft/vscode/extensions/theme-defaults/themes/')
-document.body.style.background = '#1E1E1E'
-document.body.style.color = '#D4D4D4'
 const highlighter = new Highlighter(langInfoArray, theme)
 document.body.append(await highlighter.highlightToElement(example, 'md', false, document))
 document.body.append(highlighter.textToPlainElement(example, false, document))
